c62a4dbacba69d36e501b3160ba4d652afd2bd39
igorlakic2 <igor.lakic2@gmail.com>
2020-10-01 02:37:17 +0200

    skills page - slider

--- script.js
+++ script.js
@@ -26,17 +26,17 @@ window.addEventListener('load', () => {
 });
 
 window.addEventListener('scroll', () => {
-    let skillsDivPosition = document.querySelector(".skills").offsetTop;
-    let skillsDivHeight = document.querySelector(".skills").offsetHeight;
+    let skillsDivPosition = document.querySelector("#skills").offsetTop;
+    let skillsDivHeight = document.querySelector("#skills").offsetHeight;
     let scrollPx = window.pageYOffset + window.innerHeight;
 
-    if(scrollPx > skillsDivPosition + (skillsDivHeight / 2)){
-        setTimeout(() => skillsAnimation('html'), 200);
-        setTimeout(() => skillsAnimation('css'), 500);
-        setTimeout(() => skillsAnimation('js'), 800);
-        setTimeout(() => skillsAnimation('rjs'), 1100);
-        setTimeout(() => skillsAnimation('php'), 1400);
-    }
+    // if(scrollPx > skillsDivPosition + (skillsDivHeight / 2)){
+    //     setTimeout(() => skillsAnimation('html'), 200);
+    //     setTimeout(() => skillsAnimation('css'), 500);
+    //     setTimeout(() => skillsAnimation('js'), 800);
+    //     setTimeout(() => skillsAnimation('rjs'), 1100);
+    //     setTimeout(() => skillsAnimation('php'), 1400);
+    // }
 });
 
 let imgs = document.querySelectorAll('.img');
@@ -50,3 +50,44 @@ for(let i=0; i<imgs.length; i++){
         document.querySelectorAll('.bg')[i].style.transform = 'translate(0, 300px)';
     })
 }
+
+let skills = [...document.querySelectorAll(".skill")];
+let skillsDiv = document.querySelector(".skillsDiv");
+let ppp = document.querySelectorAll(".skill p");
+
+const skillsSlider = (skills) => {        
+    document.querySelector(".skillsDiv").innerHTML = "";
+
+    for(let i=0; i<skills.length; i++){  
+        for(let j=1; j<=3; j++){
+            skills[j].childNodes[1].style.color = '#0b56a4';
+            skills[j].childNodes[1].style.fontSize = '130px';
+            skills[j].style.opacity = 1;
+            skills[i].children[1].style.opacity = 1;
+        }  
+        skills[0].childNodes[1].style.color = 'lightslategray';
+        skills[0].childNodes[1].style.fontSize = '90px';
+        skills[0].style.opacity = 0.5;
+        skills[0].children[1].style.opacity = 0;
+        skills[4].childNodes[1].style.color = 'lightslategray';
+        skills[4].childNodes[1].style.fontSize = '90px';
+        skills[4].style.opacity = 0.5;
+        skills[4].children[1].style.opacity = 0;
+    
+        skills.forEach(skill => skillsDiv.appendChild(skill)); 
+    }
+   
+}
+
+const plusSlides = (n) => {
+    let skillsArr = [...document.querySelectorAll(".skill")];
+
+    if(n == 1){
+        skillsArr = [skillsArr[1], skillsArr[2], skillsArr[3], skillsArr[4], skillsArr[0]];
+    }else if(n == -1){        
+        skillsArr = [skillsArr[4], skillsArr[0], skillsArr[1], skillsArr[2], skillsArr[3]];
+    }
+    skillsSlider(skillsArr);
+}
+
+skillsSlider(skills);
